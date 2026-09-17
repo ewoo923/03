ffedfbdcbc8afa30435b6d0b406898f91838d248
ewoo923 <ewoo923@sookmyung.ac.kr>
2026-09-17 16:26:14 +0900

    2213107_260917

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    char c;
+    int i;
+    
+    printf("enter a character :");
+    scanf("%c", &c);
+    
+    i = c - '0';
+    printf("The input number is %i\n", i);
+    return 0;
+}
